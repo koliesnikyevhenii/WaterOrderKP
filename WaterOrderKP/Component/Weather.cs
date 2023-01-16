25f7629f2aa6dbf395c866342292ef10541b2220
koliesnikyevhenii <yevhenii.ko@ideo-digital.com>
2023-01-16 19:52:29 +0200

    add component weather

---
+++ WaterOrderKP/Component/Weather.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WaterOrderKP.Models;
+
+namespace WaterOrderKP.Component
+{   
+    public class WeatherViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            WeatherModel model = new WeatherModel();
+            model.TemperatureC = 30;
+            model.Summary = "ball";
+            model.Date = DateTime.Now;
+            // model api request 
+
+            return View("WeatherBlock", model);
+        }
+    }
+}
